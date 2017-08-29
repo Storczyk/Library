22ffdf23a75468637aa040c1d7f90ef0ba9a90ed
Yeavein <yeavein@gmail.com>
2017-08-29 14:38:44 +0200

    Commands added

---
+++ Library/Application/Commands/DeleteBook/DeleteBookCommandHandler.cs
@@ -0,0 +1,12 @@
+﻿using Library.Application.General;
+
+namespace Library.Application.Commands.DeleteBook
+{
+    public class DeleteBookCommandHandler : ICommandHandler<DeleteBookCommand>
+    {
+        public void Handle(DeleteBookCommand command)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
---
+++ Library/Application/Commands/EditBook/EditBookCommandHandler.cs
@@ -0,0 +1,13 @@
+﻿using Library.Application.General;
+using System;
+
+namespace Library.Application.Commands.EditBook
+{
+    public class EditBookCommandHandler : ICommandHandler<EditBookCommand>
+    {
+        public void Handle(EditBookCommand command)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
