d3c184442697ef267bde9e2a073e98f47af594b7
Yeavein <yeavein@gmail.com>
2017-08-30 10:32:52 +0200

    JS added

--- Library.Web/Areas/Admin/Controllers/BookController.cs
+++ Library.Web/Areas/Admin/Controllers/BookController.cs
@@ -11,7 +11,7 @@ using System.Collections.Generic;
 namespace Library.Web.Areas.Admin.Controllers
 {
     [Area("Admin")]
-    [Authorize(Roles = "Administrator")]
+    //[Authorize(Roles = "Administrator")]
     public class BookController : BaseController
     {
         public BookController(ICommandBus commandBus, IQueryDispatcher queryDispatcher) : base(commandBus, queryDispatcher) { }
@@ -56,10 +56,8 @@ namespace Library.Web.Areas.Admin.Controllers
         }
 
         [HttpGet]
-        [Route("/Admin/Book/Edit/{id}")]
-        public IActionResult Edit(string id)
+        public IActionResult Edit(GetBookQuery getBookQuery)
         {
-            var getBookQuery = new GetBookQuery { Id = id };
             var book = queryDispatcher.Dispatch<GetBookQuery, BookQuery>(getBookQuery);
 
             return View(book);
