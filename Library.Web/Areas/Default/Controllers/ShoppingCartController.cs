13b78c31becbc91b2c03686a48c911c133207083
Yeavein <yeavein@gmail.com>
2017-09-01 16:15:24 +0200

    no message

--- Library.Web/Areas/Default/Controllers/ShoppingCartController.cs
+++ Library.Web/Areas/Default/Controllers/ShoppingCartController.cs
@@ -3,11 +3,6 @@ using Library.Application.General;
 using Library.Application.Queries.Books;
 using Library.Application.Queries.Cart;
 using Library.Application.Queries.Order;
-<<<<<<< HEAD
-using Library.Web.Areas.Default.ViewModels;
-using Library.Web.Extensions;
-=======
->>>>>>> c11fede6ec67dde7c8eec4922935efaabfb6c406
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
@@ -16,7 +11,6 @@ using System.Linq;
 
 namespace Library.Web.Areas.Default.Controllers
 {
-
     public class ShoppingCartController : BaseController
     {
         public ShoppingCartController(ICommandBus commandBus, IQueryDispatcher queryDispatcher) : base(commandBus, queryDispatcher) { }
@@ -36,6 +30,7 @@ namespace Library.Web.Areas.Default.Controllers
             addToCartCommand.CurrentSession = HttpContext.Session;
             commandBus.Send(addToCartCommand);
         }
+
         [Route("[controller]/[action]")]
         [HttpPost]
         public void RemoveFromCart([FromBody] RemoveFromCartCommand removeFromCartCommand)
