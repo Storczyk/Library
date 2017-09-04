e4fa1f46b49377459490d2c92aabbe21132f6dd8
PGS-SOFT\psroczyk <psroczyk@pgs-soft.com>
2017-09-04 09:07:22 +0200

    no message

---
+++ Library.Web/Areas/Default/Controllers/OrdersControllers.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Library.Application.General;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Authorization;
+
+namespace Library.Web.Areas.Default.Controllers
+{
+    [Authorize]
+    public class OrdersControllers : BaseController
+    {
+        public OrdersControllers(ICommandBus commandBus, IQueryDispatcher queryDispatcher) : base(commandBus, queryDispatcher)
+        {
+        }
+
+        [Route("[controller]/[action]")]
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
