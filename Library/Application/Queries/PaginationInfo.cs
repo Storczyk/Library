1003ea229aec51e6a7baebbdbd776d12e247e8f6
PGS-SOFT\psroczyk <psroczyk@pgs-soft.com>
2017-09-06 09:04:10 +0200

    no message

---
+++ Library/Application/Queries/PaginationInfo.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Library.Application.Queries
+{
+    public abstract class PaginationInfo
+    {
+        public int Page { get; set; }
+        public int HowManyPages { get; set; }
+        public int PageSize { get; set; }
+    }
+}
