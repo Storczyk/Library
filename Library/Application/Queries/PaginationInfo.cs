83f2435a479a9094e0884a2233717aea8b2898b2
PGS-SOFT\psroczyk <psroczyk@pgs-soft.com>
2017-09-06 13:57:24 +0200

    no message

--- Library/Application/Queries/PaginationInfo.cs
+++ Library/Application/Queries/PaginationInfo.cs
@@ -8,21 +8,22 @@ namespace Library.Application.Queries
     public class PaginatedList<T> : List<T>
     {
         public PaginationInfo PaginationInfo { get; set; }
-        public PaginatedList(List<T> items, int count, int pageIndex, int pageSize, string bookTitle = null, Genre? genre = null)
+        public PaginatedList(List<T> items, int count, int pageIndex, int pageSize, string bookTitle = null, Genre? genre = null, string userId = null)
         {
             PaginationInfo = new PaginationInfo();
             PaginationInfo.PageIndex = pageIndex;
             PaginationInfo.TotalPages = (int)Math.Ceiling(count / (double)pageSize);
             PaginationInfo.BookTitle = bookTitle;
             PaginationInfo.Genre = genre;
+            PaginationInfo.UserId = userId;
             this.AddRange(items);
         }
 
-        public static PaginatedList<T> Create(IQueryable<T> source, int pageIndex, int pageSize, string bookTitle = null, Genre? genre = null)
+        public static PaginatedList<T> Create(IQueryable<T> source, int pageIndex, int pageSize, string bookTitle = null, Genre? genre = null, string userId = null)
         {
             var count = source.Count();
             var items = source.Skip((pageIndex - 1) * pageSize).Take(pageSize).ToList();
-            return new PaginatedList<T>(items, count, pageIndex, pageSize, bookTitle, genre);
+            return new PaginatedList<T>(items, count, pageIndex, pageSize, bookTitle, genre, userId);
         }
 
     }
@@ -35,6 +36,7 @@ namespace Library.Application.Queries
         public bool HasNextPage { get { return PageIndex < TotalPages; } }
         public string BookTitle { get; set; }
         public Genre? Genre { get; set; }
+        public string UserId { get; set; }
     }
 
 }
